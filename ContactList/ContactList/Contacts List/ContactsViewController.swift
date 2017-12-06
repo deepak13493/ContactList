c798f11e5353d1aab0f26ff9fed5505513ac6fe3
Deepak Sharma <deepak@Deepaks-MacBook-Pro.local>
2017-12-06 11:42:24 +0530

    correct api url string

--- ContactList/ContactList/Contacts List/ContactsViewController.swift
+++ ContactList/ContactList/Contacts List/ContactsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 struct URLConstant {
-    static let apiPath = "http://jsonplaceholder.typicode.com/userss"
+    static let apiPath = "http://jsonplaceholder.typicode.com/users"
 }
 
 class ContactsViewController: BaseViewController {
